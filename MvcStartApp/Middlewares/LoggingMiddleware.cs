d701e193d2565a6180b0de5ae95ef72197392873
Ivan <ivanshmytov@mail.ru>
2022-10-30 12:14:59 +0400

    Добавьте файлы проекта.

---
+++ MvcStartApp/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using MvcStartApp.Models.Db;
+using System;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogRepository _repo;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next, ILogRepository repo)
+        {
+            _next = next;
+            _repo = repo;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke  или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Для логирования данных о запросе используем свойста объекта HttpContext
+            Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+            // Передача запроса далее по конвейеру
+            var req = new Request()
+            {
+                Id = new Guid(),
+                Date = DateTime.Now,
+                Url = $"{context.Request.Host.Value + context.Request.Path}"
+            };
+            await _repo.AddRequest(req);
+            await _next.Invoke(context);
+        }
+    }
+}
---
+++ MvcStartApp/Models/Db/LogRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Models.Db
+{
+    public class LogRepository : ILogRepository
+    {
+        private readonly BlogContext _context;
+
+        public LogRepository(BlogContext context)
+        {
+            _context = context;
+        }
+        public async Task AddRequest(Request request)
+        {
+            request.Date = DateTime.Now;
+            request.Id = Guid.NewGuid();
+            var entry = _context.Entry(request);
+            if (entry.State == EntityState.Detached)
+            {
+                await _context.Requests.AddAsync(request);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<Request[]> GetLogs()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
